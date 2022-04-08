874a88dc98d4253ae3f401add0c5608affa03fc5
Rebe1li0n <simidawhu@gmail.com>
2022-04-09 00:07:10 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import  HttpResponse
 
 # Create your views here.
+def index(request):
+    line1 = '<h1 style= "text-align: center">Warlock_Brawl</h1>'
+    line2 = '<img src="https://assets.dicebreaker.com/0-dungeons-and-dragons-warlock-5e-guide.png/BROK/resize/1920x1920%3E/format/jpg/quality/80/0-dungeons-and-dragons-warlock-5e-guide.png">'
+    return HttpResponse(line1 + line2);
+
